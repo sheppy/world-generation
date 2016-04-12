209dc7c1af661f1a7b954918a65d89cc63183bf0
Chris Sheppard <chris.sheppard@thebioagency.com>
2016-04-12 12:59:38 +0100

    Elevations

--- src/js/Map.js
+++ src/js/Map.js
@@ -12,6 +12,11 @@ class Map {
         this.width = options.width;
         this.height = options.height;
 
+        this.windNoiseSize = options.windNoiseSize || 3;
+        this.windContinentNoiseSize = options.windContinentNoiseSize || 8;
+        this.windBandWeight = options.windBandWeight || 0.8;
+        this.windContinentWeight = options.windContinentWeight || 0.3;
+
         this.initMapData();
 
         this.generateHeightMap(options.noiseMapCount, options.elevations);
@@ -65,7 +70,7 @@ class Map {
 
     generateWindMap() {
         // 1. Start with a base noise map. (FBM octaves = 5.0 and size = 4.0)
-        this.windNoiseMaps = Noise.generateNoiseMaps(this.random, this.width, this.height, 4);
+        this.windNoiseMaps = Noise.generateNoiseMaps(this.random, this.width, this.height, this.windNoiseSize);
         this.windNoiseMap = Noise.combineNoiseMapsWeighted(this.width, this.height, this.windNoiseMaps);
 
         // 2. Define bands where with varying wind values. (Strong-Weak-Strong for this example)
@@ -75,7 +80,7 @@ class Map {
         let mapLength = this.width * this.height;
 
         for (let i = 0; i < mapLength; i++) {
-            this.windNoiseMap[i] += bands[i] * 0.5;
+            this.windNoiseMap[i] += bands[i] * this.windBandWeight;
         }
 
         // 4. To complete the base map the whole map is normalized between 0.0 and a base weight.
@@ -85,14 +90,24 @@ class Map {
         }
 
         // 5. A second noise map is created called the continent noise map. (FBM octaves = 5.0 and size = 8.0)
-        this.continentNoiseMaps = Noise.generateNoiseMaps(this.random, this.width, this.height, 8);
+        this.continentNoiseMaps = Noise.generateNoiseMaps(this.random, this.width, this.height, this.windContinentNoiseSize);
         this.continentNoiseMap = Noise.combineNoiseMapsWeighted(this.width, this.height, this.continentNoiseMaps);
 
         // 6. Using the Voronoi operation create a map where every cell has a value equal to its distance to the nearest coast.
         // 7. For each cell I create a weight based on the distance to the coast. If the point is further than the distance threshold the weight is 1.0 if over ocean and 0.0 if over land.
-        // 8. This weight is combines the the continent weight and then multiplied by the continent noise and added to the base map.
-        // 9. The final map is then normalized.
 
+
+        // 8. This weight is combined with the continent weight and then multiplied by the continent noise and added to the base map.
+        this.windMap = [];
+        for (let i = 0; i < mapLength; i++) {
+            this.windMap[i] = this.windNoiseMap[i] + (this.continentNoiseMap[i] * this.windContinentWeight);
+        }
+
+        // 9. The final map is then normalized.
+        largestVal = this.windMap.reduce((largest, val) => val > largest ? val : largest, 0);
+        for (let i = 0; i < mapLength; i++) {
+            this.windMap[i] = this.windMap[i] / largestVal;
+        }
 
         // TODO: Update the main map data
     }
@@ -105,27 +120,61 @@ class Map {
         elevations = JSON.parse(JSON.stringify(elevations));
         heightMap = heightMap.slice(0).sort();
 
+        let seaLevel = heightMap[Math.floor(0.6 * heightMap.length)];
+        let skyLevel = heightMap[heightMap.length - 1];
+
+
         for (let elevation of elevations) {
-            elevation.index = Math.ceil(elevation.value * heightMap.length);
+            let value;
+
+            if (elevation.hasOwnProperty("seaLevel")) {
+                value = seaLevel + (seaLevel * (elevation.seaLevel / 255));
+            } else if (elevation.hasOwnProperty("skyLevel")) {
+                value = skyLevel + (skyLevel + (elevation.skyLevel / 255));
+            } else {
+                value = elevation.level / 255;
+            }
+
+            elevation.index = Math.max(0, Math.ceil(value * heightMap.length));
 
             if (elevation.index >= heightMap.length) {
                 elevation.index = heightMap.length - 1;
             }
 
-            elevation.value = heightMap[elevation.index];
+            if (elevation.hasOwnProperty("seaLevel") || elevation.hasOwnProperty("skyLevel")) {
+                elevation.value = heightMap[elevation.index];
+            } else {
+                elevation.value = value;
+            }
         }
+
+        console.log(elevations);
 
         return elevations;
     }
 
     getElevationFromHeight(height) {
-        for (let i = this.elevations.length - 1; i >= 0; --i) {
-            let elevation = this.elevations[i];
 
-            if (height >= elevation.value) {
+        for (let i = 0, j = this.elevations.length - 1; i < j; i++) {
+            let elevation = this.elevations[i];
+            let nextElevation = this.elevations[i + 1];
+
+            if (height < nextElevation.value) {
                 return i;
             }
         }
+
+
+        return this.elevations.length - 1;
+
+
+        // for (let i = this.elevations.length - 1; i >= 0; --i) {
+        //     let elevation = this.elevations[i];
+        //
+        //     if (height >= elevation.value) {
+        //         return i;
+        //     }
+        // }
     }
 
     createBandMap(numBands) {
--- src/js/index.js
+++ src/js/index.js
@@ -16,33 +16,55 @@ let settings = {
     height: HEIGHT,
     elevations: [
         {
-            name: "min",
-            value: 0,
+            name: "deepSea",
+            seaLevel: -255,
             color: [48, 96, 130]
         },
         {
             name: "sea",
-            value: 0.25,
+            seaLevel: -40,
             color: [99, 155, 255]
         },
         {
-            name: "land",
-            value: 0.45,
+            name: "beach",
+            seaLevel: 15,
+            color: [251, 242, 54]
+        },
+        {
+            name: "plain",
+            seaLevel: 35,
             color: [106, 190, 48]
         },
         {
-            name: "hill",
-            value: 0.8,
+            name: "forest",
+            seaLevel: 205,
             color: [75, 105, 47]
         },
         {
+            name: "hill",
+            // value: 0.8,
+            // level: 205,
+            skyLevel: -10,
+            color: [138, 111, 48]
+            // color: [0, 255, 255]
+        },
+        {
             name: "mountain",
-            value: 1,
+            // value: 1,
+            // level: 230,
+            skyLevel: -1,
             color: [255, 255, 255]
         }
     ],
 
-    renderMode: "windNoiseMap"
+    // Wind
+    windNoiseSize: 3,
+    windContinentNoiseSize: 8,
+    windBandWeight: 0.8,
+    windContinentWeight: 0.3,
+
+    // Rendering debugging
+    renderMode: "windMap"
     // renderMode: "data"
 };
 
@@ -53,6 +75,7 @@ let renderModes = [
     "heightRollingMask",
     "heightNoiseMap",
     "windNoiseMap",
+    "windMap",
     "continentNoiseMap"
 ];
 
@@ -70,7 +93,8 @@ function render() {
         case "heightRollingMask":   Graphics.renderAlphaMap(ctx, m.heightRollingMask, WIDTH, HEIGHT); break;
         case "heightMap":           Graphics.renderAlphaMap(ctx, m.heightMap, WIDTH, HEIGHT); break;
         case "windNoiseMap":        Graphics.renderAlphaMap(ctx, m.windNoiseMap, WIDTH, HEIGHT); break;
-        case "continentNoiseMap":        Graphics.renderAlphaMap(ctx, m.continentNoiseMap, WIDTH, HEIGHT); break;
+        case "continentNoiseMap":   Graphics.renderAlphaMap(ctx, m.continentNoiseMap, WIDTH, HEIGHT); break;
+        case "windMap":             Graphics.renderAlphaMap(ctx, m.windMap, WIDTH, HEIGHT); break;
 
         case "dataFlat":            Graphics.renderHeightMapData(ctx, m, true); break;
 
@@ -91,20 +115,24 @@ var gui = new dat.GUI();
 gui.add(settings, "seed").onFinishChange(update);
 gui.add(settings, "noiseMapCount").onFinishChange(update);
 gui.add(settings, "renderMode", renderModes).onFinishChange(render);
+gui.add(settings, "windNoiseSize").onFinishChange(update);
+gui.add(settings, "windContinentNoiseSize").onFinishChange(update);
+gui.add(settings, "windBandWeight").onFinishChange(update);
+gui.add(settings, "windContinentWeight").onFinishChange(update);
 
 
-var seaFolder = gui.addFolder("Sea");
-seaFolder.add(settings.elevations.find(ele => ele.name === "sea"), "value", 0, 1).onFinishChange(update);
-seaFolder.open();
-
-var landFolder = gui.addFolder("Land");
-landFolder.add(settings.elevations.find(ele => ele.name === "land"), "value", 0, 1).onFinishChange(update);
-landFolder.open();
-
-var hillsFolder = gui.addFolder("Hills");
-hillsFolder.add(settings.elevations.find(ele => ele.name === "hill"), "value", 0, 1).onFinishChange(update);
-hillsFolder.open();
-
-var mountainsFolder = gui.addFolder("Mountains");
-mountainsFolder.add(settings.elevations.find(ele => ele.name === "mountain"), "value", 0, 1).onFinishChange(update);
-mountainsFolder.open();
+// var seaFolder = gui.addFolder("Sea");
+// seaFolder.add(settings.elevations.find(ele => ele.name === "sea"), "seaLevel", -255, 255).onFinishChange(update);
+// seaFolder.open();
+//
+// var landFolder = gui.addFolder("Land");
+// landFolder.add(settings.elevations.find(ele => ele.name === "plain"), "seaLevel", -255, 255).onFinishChange(update);
+// landFolder.open();
+//
+// var hillsFolder = gui.addFolder("Hills");
+// hillsFolder.add(settings.elevations.find(ele => ele.name === "hill"), "level", -255, 255).onFinishChange(update);
+// hillsFolder.open();
+//
+// var mountainsFolder = gui.addFolder("Mountains");
+// mountainsFolder.add(settings.elevations.find(ele => ele.name === "mountain"), "level", -255, 255).onFinishChange(update);
+// mountainsFolder.open();
